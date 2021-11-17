0ddbecc506166c6ad207440ad117ca70d22df5a1
dzozrawr <jovan_96@live.ru>
2021-11-17 17:05:41 +0100

    added phase 1 and half of phase 2 skeletons

---
+++ Assets/_Game/Scripts/AddForceOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceOnClick : MonoBehaviour
+{
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {                
+                hit.rigidbody.AddForceAtPosition(-Vector3.forward*50, hit.point);
+            }
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/CameraTransitions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransitions : MonoBehaviour
+{
+    public Transform[] views;
+    [SerializeField] private float transitionSpeed;
+    private Transform currentView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentView = transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            currentView = views[0];
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position,currentView.position,Time.deltaTime*transitionSpeed);
+
+        Vector3 currentAngle = new Vector3(
+            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed)
+            );
+        transform.eulerAngles = currentAngle;
+    }
+}
